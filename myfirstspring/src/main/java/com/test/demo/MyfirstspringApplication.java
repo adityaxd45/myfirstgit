5ae255fb1c54aa1b780f3c9e5961428f0f8689a2
Aditya samal <adityasamal589@gmail.com>
2025-07-07 08:21:03 +0530

    My first commit

---
+++ myfirstspring/src/main/java/com/test/demo/MyfirstspringApplication.java
@@ -0,0 +1,15 @@
+package com.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstspringApplication.class, args);
+		
+		System.out.println("hello world");
+	}
+
+}
